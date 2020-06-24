6dc076904c7b656e9a6acebcc3fda72fd7742c3b
masc1005 <masc1005@gmail.com>
2020-06-24 15:02:31 -0300

    Rotas no app.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/Routes'
 
 export default function App() {
   return (
-    
+    <Routes />
   );
 }

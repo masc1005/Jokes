5627f7e822ad58b5f15e25b38e0f7d5376f88bfa
masc1005 <masc1005@gmail.com>
2020-06-24 15:10:43 -0300

    comentarios no projeto

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Routes from './src/Routes'
+import Routes from './src/Routes' // importação das rotas
 
 export default function App() {
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
-import axios from 'axios'
+import axios from 'axios' //importação da depedência axios
 
-const api = axios.create({
+
+// dando todo o valor da api para a variavel api
+const api = axios.create({ 
     baseURL: 'https://official-joke-api.appspot.com/jokes/random'
 })
 
-export default api
+
+export default api //exportando variavel
